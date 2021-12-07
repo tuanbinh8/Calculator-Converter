55e26795d6aa30e1978fae628ff303e05917d856
tuanbinh8 <tuanbinh1209@gmail.com>
2021-12-07 17:42:05 +0700

    update some code

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.getElementById('switching-mode-button').onclick=()=>{
-    
+document.getElementById('switching-mode-button').onclick = () => {
+
+}
+let selectOtherCalculatorsButtons = Array.from(document.querySelectorAll('#other-calculators div'))
+for (let i = 0; i < selectOtherCalculatorsButtons.length; i++) {
+    let clickedButton = selectOtherCalculatorsButtons[i]
+    clickedButton.onclick = () => {
+        selectOtherCalculatorsButtons.map((button) => {
+            if (button !== clickedButton)
+                button.classList.remove('active')
+            else
+                button.classList.add('active')
+        })
+    }
+
 }
